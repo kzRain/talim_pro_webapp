c9c6acaba036d2a6771f32e367b4cfd129a19e42
rus <ruslan.beyganov@gmail.com>
2023-03-07 17:53:34 +0600

    fixing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
+const tg = window.Telegram.WebApp;
 </script>
 
 <template>
   <div>
     <p>Work</p>
-    <button onclick="tg.close();">Закрыть</button>
+    <button @click="tg.close()">Закрыть</button>
   </div>
 </template>
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,5 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
-  const tg = window.Telegram.WebApp;
   export default component
 }
