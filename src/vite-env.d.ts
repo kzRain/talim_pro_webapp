0de781e7af3f31a4684eafc7fd058eb79ca070f7
rus <ruslan.beyganov@gmail.com>
2023-03-07 17:32:34 +0600

    Inital commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  const tg = window.Telegram.WebApp;
+  export default component
+}
