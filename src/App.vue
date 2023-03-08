9a85267649a0fe1840585f7a6a1a34e2039ea04d
rus <ruslan.beyganov@gmail.com>
2023-03-08 23:43:24 +0600

    changes for inline button works

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ const onSendData = () => {
       },
       body: JSON.stringify(data)
     };
-    tg.MainButton.text = data.city;
+    tg.MainButton.text = "Start";
     fetch('http://devtg.courstore.com/web-app', requestOptions)
     //     .then((response) => {
     //           console.log(response);
